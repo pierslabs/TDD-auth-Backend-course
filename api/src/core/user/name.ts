c4bd0fb42ca2722f07075866e2e6c5481c44c149
pierslabs <pedrolosasp@gmail.com>
2023-06-12 20:16:59 +0200

    refactor name

--- api/src/core/user/name.ts
+++ api/src/core/user/name.ts
@@ -1,17 +1,26 @@
 class Name {
+  public MIN_LENGHT: number = 2;
+  public MAX_LENGHT: number = 25;
   constructor(public readonly value: string) {
     this.validateOrThrowException(value);
   }
 
   private validateOrThrowException(value: string) {
-    // only letters and spaces /^[a-zA-Z ]+$/
-    const invalidLength = value.length < 2 || value.length > 25;
-    const invalidFormat = /^[a-zA-Z][a-zA-Z\s]*[a-zA-Z]$/.test(value) === false;
+    const invalidLength = this.isValidLength(value);
+    const invalidFormat = this.isValidFormat(value);
 
     if (invalidLength || invalidFormat) {
       throw new Error();
     }
   }
+
+  private isValidLength = (value: string): boolean => {
+    return value.length < this.MIN_LENGHT || value.length > this.MAX_LENGHT;
+  };
+
+  private isValidFormat = (value: string): boolean => {
+    return /^[a-zA-Z][a-zA-Z\s]*[a-zA-Z]$/.test(value) === false;
+  };
 }
 
 export default Name;
