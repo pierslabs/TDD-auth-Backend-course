d680f299b4a914be0568e4954958c1fee866935c
pierslabs <pedrolosasp@gmail.com>
2023-06-15 07:28:25 +0200

    add to user, name && email class

--- api/src/core/user/user.ts
+++ api/src/core/user/user.ts
@@ -1,5 +1,8 @@
+import Email from "./email";
+import Name from "./name";
+
 class User {
-  constructor(public name: string, public email: string) {}
+  constructor(public name: Name, public email: Email) {}
 }
 
 export default User;
