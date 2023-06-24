72d060b58b08400b9a01b4a126bf713829f822f8
pierslabs <pedrolosasp@gmail.com>
2023-06-24 09:40:10 +0200

    add first exception

---
+++ api/tests/core/user/ivalid-user-name-exception.spec.ts
@@ -0,0 +1,7 @@
+import InvalidUserNameException from "../../../src/core/domain/exception/invalid-user-name-exception";
+describe("Invalid name exception", () => {
+  it("should create name without value", () => {
+    const sut: InvalidUserNameException = new InvalidUserNameException();
+    expect(sut).toBeInstanceOf(Error);
+  });
+});
